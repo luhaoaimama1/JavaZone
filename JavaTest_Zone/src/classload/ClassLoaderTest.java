e01bf2be13c2d19e6bb732162f47f70df03caebd
fuzhipeng <1149324777@qq.com>
2017-03-14 15:50:18 +0800

    面试

--- JavaTest_Zone/src/classload/ClassLoaderTest.java
+++ JavaTest_Zone/src/classload/ClassLoaderTest.java
@@ -29,21 +29,6 @@ public class ClassLoaderTest {
      */
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         ClassLoader myLoader = new ClassLoader() {
-//            @Override
-//            public Class<?> loadClass(String name) throws ClassNotFoundException {
-//                String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
-//                InputStream is = getClass().getResourceAsStream(fileName);
-//                if (is == null)
-//                    return super.loadClass(name);
-//                try {
-//                    byte[] b = new byte[is.available()];
-//                    is.read(b);
-//                    return defineClass(name, b, 0, b.length);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                return super.loadClass(name);
-//            }
             @Override
             public Class<?> loadClass(String name) throws ClassNotFoundException {
 //                Class<?> c = findLoadedClass(name);
